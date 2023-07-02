5507384d6bfd5fe04ed30cda4d2ad67cc8a8a110
eugene huang <e@huang.org>
2023-07-02 14:09:43 -0700

    install eslint for ts

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 .PHONY: run-backend
 run-backend-dev:
 	(cd backend; npm run dev)
+
+.PHONY: lint
+lint:
+	(cd backend; npm run lint)
--- backend/index.ts
+++ backend/index.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response } from 'express';
+import express from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
