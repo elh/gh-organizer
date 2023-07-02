79d5d0b9101b767aea74329e884914ee74eef890
eugene huang <e@huang.org>
2023-07-02 14:38:12 -0700

    make gh API requests with octokit

--- backend/index.ts
+++ backend/index.ts
@@ -1,13 +1,34 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { Octokit } from "octokit";
 
+// env vars
 dotenv.config();
+if (!process.env.GH_TOKEN || !process.env.GH_ORG) {
+  console.error('GH_TOKEN and GH_ORG env vars are required');
+  process.exit(1);
+}
 
+// set up
 const app = express();
 const port = process.env.PORT || 3000;
+const octokit = new Octokit({
+  auth: process.env.GH_TOKEN,
+});
 
-app.get('/', (req, res) => {
-  res.json({"foo": "bar"});
+// app + routes
+app.get('/', async (req, res) => {
+  req.query
+  try {
+    const resp = await octokit.request("GET /orgs/{org}/members", {
+      org: process.env.GH_ORG || "",
+      per_page: 100
+    });
+    res.json(resp);
+  } catch(err) {
+    console.error(err);
+    res.status(500).json({'error': 'Error querying Github'});
+  }
 });
 
 app.listen(port, () => {
