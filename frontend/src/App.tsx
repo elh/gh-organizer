56fd4e650139d5acedf36a4fd8e21643be874c80
eugene huang <e@huang.org>
2023-07-02 19:03:53 -0700

    install tailwind. clear default styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container mx-auto">
+          <div className="flex flex-row flex-wrap py-4">
+              <aside className="w-full sm:w-1/3 md:w-1/4 px-2">
+                  <div className="sticky top-0 p-4 w-full">
+                      <ul className="flex flex-col overflow-hidden">
+                          A
+                      </ul>
+                      <ul className="flex flex-col overflow-hidden">
+                          B
+                      </ul>
+                      <ul className="flex flex-col overflow-hidden">
+                          C
+                      </ul>
+                  </div>
+              </aside>
+              <main role="main" className="w-full sm:w-2/3 md:w-3/4 pt-1 px-2">
+                Hello
+              </main>
+          </div>
+      </div>
     </div>
   );
 }
