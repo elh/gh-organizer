b01e8186da3942f77319784ed628949f7da877d8
eugene huang <e@huang.org>
2023-07-02 18:02:47 -0700

    use graphql. /org and /members routes

--- backend/index.ts
+++ backend/index.ts
@@ -17,14 +17,112 @@ const octokit = new Octokit({
 });
 
 // app + routes
-app.get('/', async (req, res) => {
-  req.query
+app.get('/org', async (_, res) => {
   try {
-    const resp = await octokit.request("GET /orgs/{org}/members", {
-      org: process.env.GH_ORG || "",
-      per_page: 100
+    const resp = await octokit.request("POST /graphql", {
+      query: `query ($login: String!) {
+        organization(login: $login) {
+          avatarUrl
+          description
+          name
+        }
+      }`,
+      variables: {
+        login: process.env.GH_ORG,
+      },
     });
     res.json(resp);
+  } catch(err) {
+    console.error(err);
+    res.status(500).json({'error': 'Error querying Github'});
+  }
+});
+
+// query params:
+// * cursor - cursor from pageInfo.endCursor
+app.get('/members', async (req, res) => {
+  const pageSize = 20;
+  // no pull request information
+  try {
+    const resp = await octokit.request("POST /graphql", {
+      query: `query ($login: String!, $after: String, $pageSize: Int!) {
+        organization(login: $login) {
+          membersWithRole(first: $pageSize, after: $after) {
+            totalCount
+            pageInfo {
+              endCursor
+              hasNextPage
+            }
+            nodes {
+              # bio
+              login
+              name
+              avatarUrl
+              pronouns
+              bio
+              status {
+                emoji
+                message
+              }
+              email
+              twitterUsername
+              websiteUrl
+              # attributes. not querying pull requests here. just some other simple stats
+              followers {
+                totalCount
+              }
+              following {
+                totalCount
+              }
+              organizations(first: 100) {
+                totalCount
+                pageInfo {
+                  endCursor
+                  hasNextPage
+                }
+                nodes {
+                  login
+                  name
+                }
+              }
+              repositories(isFork: false, privacy: PUBLIC) {
+                totalCount
+              }
+              socialAccounts(first: 100) {
+                totalCount
+                pageInfo {
+                  endCursor
+                  hasNextPage
+                }
+                nodes {
+                  displayName
+                  provider
+                  url
+                }
+              }
+              sponsors {
+                totalCount
+              }
+              starredRepositories {
+                totalCount
+              }
+            }
+          }
+        }
+      }`,
+      variables: {
+        login: process.env.GH_ORG,
+        pageSize: pageSize,
+        after: req.query['cursor'] || null,
+      },
+    });
+    if (resp.data.errors) {
+      console.error(resp.data.errors);
+      res.status(500).json({'error': 'Error querying Github'});
+      return;
+    }
+    const out = resp.data.data.organization.membersWithRole;
+    res.json(out);
   } catch(err) {
     console.error(err);
     res.status(500).json({'error': 'Error querying Github'});
